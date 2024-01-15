78802c8cc1725ac5836eeb8f0b67953f230bc8ea
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2024-01-15 10:45:39 +0100

    update card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,7 @@ export const Card = ({ title, description, imageData }) => {
                 <a href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                 </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Beskrivning: {description}</p>
             </div>
         </div>
     )
